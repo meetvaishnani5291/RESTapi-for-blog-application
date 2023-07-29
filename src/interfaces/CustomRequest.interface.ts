17737af522b3888d22fafeaabec8335270020b8f
meetvaishnani5291 <meet.vaishnani@simformsolutions.com>
2023-07-29 19:31:34 +0530

    Fix removed old interface file

--- src/interfaces/CustomRequest.interface.ts
+++
@@ -1,9 +0,0 @@
-import { IUser } from "../models/users.model";
-import { Request } from "express";
-import { ParsedQueryOptions } from "./queryOptions.interface";
-
-export interface CustomRequest extends Request {
-  user?: IUser;
-  blogQueryOptions?: ParsedQueryOptions
-  isPublicRoute?: boolean;
-}
